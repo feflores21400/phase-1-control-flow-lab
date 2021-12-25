d29a18cca0649727dfeffc0ae485f6f5514e5a87
Fernando <feflores21400@gmail.com>
2021-12-24 20:18:34 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <= 400){
+    return'This one is on me!'}
+    else if(distance >2000 && distance<=2500){return'I will gladly take your thirty bucks.'}
+    else if(distance >2500){return 'No can do.'}
+}
+s
+function ternaryCheckCity(city){ 
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case'generous':
+    return 'Thank you so much.'
+    case'not as generous':
+    return  'Thank you.' 
+   default:
+    return'Bye.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+ 
 }
